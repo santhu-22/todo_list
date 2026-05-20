92ddcc86bda8e81106911c9bc0e4ecbebb04b05a
santhu-22 <santhoshibehara4488@gmail.com>
2026-05-20 14:47:11 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,151 @@
+# day 4
+'''print("hello world",7)
+print("bye")
+print(5)'''
+
+#day 5
+'''print("she is a good girl and the viewer is also good girl/boy because of that the boy is sees the harry python subscripction videos and we want to learn something to special and so we want to learn newly" )
+
+# comments, escape sequences character,&print
+print("this ids execute")
+print("i am daughter\'s anand")
+
+print(object(s), sep=sepator,end=end,)'''
+
+
+#day 6
+
+'''a=True
+d="santhoshi"
+o=None
+print((a))
+print((d))
+print((o))
+
+
+a=True
+d="santhoshi"
+o=None
+print(type(a))
+print(type(d))
+print(type(o))'''
+
+#day 7,day 8
+'''a=int(input("give a number"))
+b=int(input("give b number"))
+g=a+b
+print("value of g",g)
+g=a-b
+print("value of g",g)
+g=a*b
+print("value of g",g)
+g=a/b
+print("value of g",g)
+
+list=[6,47,[56],['apple','banana']]
+print(list)
+
+a=input("enter a number")
+b=int(a)
+print(b)
+
+char_a='A'
+ascii_a=ord(char_a)
+print(ascii_a)
+
+a=65
+ascii_a=chr(a)
+print(ascii_a)'''
+ 
+#day 9 type casting
+'''a='1'
+a=int(a)
+print(type(a))
+
+a='1'
+a=tuple(a)
+print(a)
+
+num=5+(2.0)
+print(type(num))
+
+is_true=bool(11)
+is_true=bool(10)'''
+
+#day 10 taking input
+'''a=input("enter a number")
+print(a)'''
+
+#day 11, multiline strings
+'''name="harry"
+a=name
+print("lets use a if loop\n")
+if a==name:
+    print(name)
+else:
+    print(None)
+print("lets use a for loop\n")
+for i in name:
+    print(i)'''
+
+#day 12 slicing on string
+'''fruit='mango'
+print(len(fruit))
+print(fruit[:5])
+print(fruit[:5])
+print(fruit[::-1])
+print(fruit[-3:-1])
+print(fruit[-4:-2])
+print(fruit[1:4])'''
+
+'''name="santhoshhi"
+print(name)
+for i in name:
+    print(i)'''
+
+#day 13 string methods
+'''name="santhoshi"
+print(name)
+print(name.upper())             #convert the string into upper case
+print(name.lower())             #convert the string into lower case
+print(name.replace("santhoshi","sumanth"))  #it takes two strings and the 1 st string replace with 2nd string
+print(name.capitalize())        #convert the first letter into capital
+print(name.center)              #it show the center of the name
+print(name.endswith("shi"))     #it show true or false if the string end with correct words or not
+print(name.find("t"))           #it find the index of the character in a string
+print(name.index("h"))          #it show the index of the character in a string
+print(name.isalnum())           #it return true if a stirng show consists of (A-Z),(a-z),(0-9)
+print(name.isalpha())           #it  returns true if the string 
+print(name.islower())           #it returns true if only the string is lower or not
+print(name.isupper())           #it returns true if the string is only in upper case
+print(name.isprintable())       #it returns if all the given string is within the string 
+print(name.isspace())           #it returns true if the string contain white spaces
+print(name.istitle())           
+print(name.startswith("t")) 
+print(name.startswith("s"))
+print(name.swapcase())          #it swap the string into lower to upper and upper to lower
+print(name.title())'''
+
+# day 14,15 conditional statements
+'''a=int(input("enter is a age"))
+print("your age is",a)
+if(a>18):
+    print("vote karoo")
+else:
+    print("nahi vote")'''
+
+num=int(input("enter a number:"))
+if(num<0):
+    print("number is negative")
+elif(num==0):
+    print("num is special")
+elif(num==99):
+    print(" num is greater")
+else:
+    print("num is nothing to special")
+
+
+
+
+
+
